222ad9922abaaccf820f215901514fc010de9058
david <cognet.david@hotmail.fr>
2023-03-26 22:59:25 +0200

    modif script.js

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,5 +1,14 @@
+// Récupération des travaux depuis le fichier JSON / API
+
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
   .then((works) => {
     console.log(works);
   });
+
+const response = await fetch("http://localhost:5678/api/works");
+const works = await response.json();
+
+// création des balises avec createElement
+
+// rattachement des balises au DOM
