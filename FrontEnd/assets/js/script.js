cd0aee71e46ac215d6a2dab8fdfe31f5d32b6ec3
david <cognet.david@hotmail.fr>
2023-03-27 09:57:56 +0200

    !!works!!

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,37 +1,56 @@
+let works = [];
+
+const sectionPortfolio = document.querySelector("#portfolio");
+// const gallery = document.querySelector("#portfolio .gallery");
+const gallery = document.getElementsByClassName("gallery")[0];
 // Récupération des travaux depuis le fichier JSON / API
-
-fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((works) => {
-    console.log(works);
-  });
-
-const response = await fetch("http://localhost:5678/api/works");
-const works = await response.json();
+const apiWorks = async () => {
+  await fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((worksResponse) => {
+      // console.log(worksResponse);
+      works = worksResponse;
+    });
+};
+// const response = await fetch("http://localhost:5678/api/works");
+// const works = await response.json();
 
 // création des balises avec createElement
+const displayWorks = async () => {
+  await apiWorks();
+  console.log(works);
+  for (let work of works) {
+    console.log(work);
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    img.setAttribute("src", work.imageUrl);
+    img.setAttribute("alt", work.title);
 
-const article = works[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.imageUrl;
-const titleElement = document.createElement("h2");
-titleElement.innerText = article.title;
+    figure.appendChild(img);
+    gallery.appendChild(figure);
+  }
+};
+displayWorks();
 
-const categoryElement = document.createElement("p");
-categoryElement.innerText = article.categoryId;
-if (categoryId == 1) {
-  console.log("Objets");
-} else if (categoryId == 2) {
-  console.log("Appartements");
-} else if (categoryId == 2) {
-  console.log("Hotels & restaurants");
-}
+// const article = works[0];
+// const imageElement = document.createElement("img");
+// imageElement.src = article.imageUrl;
+// const titleElement = document.createElement("h2");
+// titleElement.innerText = article.title;
+// const categoryElement = document.createElement("p");
+// categoryElement.innerText = article.categoryId;
+// if (categoryId == 1) {
+//   console.log("Objets");
+// } else if (categoryId == 2) {
+//   console.log("Appartements");
+// } else if (categoryId == 2) {
+//   console.log("Hotels & restaurants");
+// }
+
 // rattachement des balises au DOM
 
-const sectionPortfolio = document.querySelector(".portfolio");
-
-sectionPortfolio.appendChild(imageElement);
-sectionPortfolio.appendChild(titleElement);
-sectionPortfolio.appendChild(categoryElement);
-sectionPortfolio.appendChild();
-sectionPortfolio.appendChild();
+// sectionPortfolio.appendChild(imageElement);
+// sectionPortfolio.appendChild(titleElement);
+// sectionPortfolio.appendChild(categoryElement);
+// sectionPortfolio.appendChild();
+// sectionPortfolio.appendChild();
