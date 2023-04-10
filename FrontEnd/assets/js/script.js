6dca502b379a6492295828d837611b6cfb4d5583
david <cognet.david@hotmail.fr>
2023-04-10 22:33:28 +0200

    uneusefull comment delation

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -79,25 +79,3 @@ const displayCategories = async () => {
   }
 };
 displayCategories();
-// const article = works[0];
-// const imageElement = document.createElement("img");
-// imageElement.src = article.imageUrl;
-// const titleElement = document.createElement("h2");
-// titleElement.innerText = article.title;
-// const categoryElement = document.createElement("p");
-// categoryElement.innerText = article.categoryId;
-// if (categoryId == 1) {
-//   console.log("Objets");
-// } else if (categoryId == 2) {
-//   console.log("Appartements");
-// } else if (categoryId == 2) {
-//   console.log("Hotels & restaurants");
-// }
-
-// rattachement des balises au DOM
-
-// sectionPortfolio.appendChild(imageElement);
-// sectionPortfolio.appendChild(titleElement);
-// sectionPortfolio.appendChild(categoryElement);
-// sectionPortfolio.appendChild();
-// sectionPortfolio.appendChild();
