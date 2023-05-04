ff7506adf1f4b3edefc508a34d007d4ec987acaf
david <cognet.david@hotmail.fr>
2023-05-04 08:13:47 +0200

    test failed affichage gallerie

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -7,3 +7,35 @@ modalTriggers.forEach((trigger) =>
 function toggleModal() {
   modalContainer.classList.toggle("active");
 }
+
+//--------------------------------------/
+
+let works = [];
+
+const galleryModal = document.querySelector(".gallery-modal");
+
+const apiWorks = async () => {
+  await fetch("http://localhost:5678/api/works")
+    .then((response) => response.json())
+    .then((worksResponse) => {
+      works = worksResponse;
+    });
+};
+
+const displayWorks = async () => {
+  await apiWorks();
+
+  for (let work of works) {
+    let figure = document.createElement("figure");
+    figure.setAttribute("data-categoryid", work.categoryId);
+    figure.setAttribute("class", "display");
+    let img = document.createElement("img");
+
+    img.setAttribute("src", work.imageUrl);
+    img.setAttribute("alt", work.title);
+
+    figure.appendChild(img);
+    galleryModal.appendChild(figure);
+  }
+};
+displayWorks();
