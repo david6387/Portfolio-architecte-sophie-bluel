7b3884da014ca9df45113316efece7c7146f97bc
david <cognet.david@hotmail.fr>
2023-05-04 08:57:50 +0200

    comment failed affichage modale

--- FrontEnd/assets/js/modale.js
+++ FrontEnd/assets/js/modale.js
@@ -10,32 +10,32 @@ function toggleModal() {
 
 //--------------------------------------/
 
-let works = [];
+// let works = [];
 
-const galleryModal = document.querySelector(".gallery-modal");
+// const galleryModal = document.querySelector(".gallery-modal");
 
-const apiWorks = async () => {
-  await fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((worksResponse) => {
-      works = worksResponse;
-    });
-};
+// const apiWorks = async () => {
+//   await fetch("http://localhost:5678/api/works")
+//     .then((response) => response.json())
+//     .then((worksResponse) => {
+//       works = worksResponse;
+//     });
+// };
 
-const displayWorks = async () => {
-  await apiWorks();
+// const displayWorks = async () => {
+//   await apiWorks();
 
-  for (let work of works) {
-    let figure = document.createElement("figure");
-    figure.setAttribute("data-categoryid", work.categoryId);
-    figure.setAttribute("class", "display");
-    let img = document.createElement("img");
+//   for (let work of works) {
+//     let figure = document.createElement("figure");
+//     figure.setAttribute("data-categoryid", work.categoryId);
+//     figure.setAttribute("class", "display");
+//     let img = document.createElement("img");
 
-    img.setAttribute("src", work.imageUrl);
-    img.setAttribute("alt", work.title);
+//     img.setAttribute("src", work.imageUrl);
+//     img.setAttribute("alt", work.title);
 
-    figure.appendChild(img);
-    galleryModal.appendChild(figure);
-  }
-};
-displayWorks();
+//     figure.appendChild(img);
+//     galleryModal.appendChild(figure);
+//   }
+// };
+// displayWorks();
