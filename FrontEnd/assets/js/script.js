1159be3dd8a15a81bd13b8818a6d804f335aebe7
david <cognet.david@hotmail.fr>
2023-05-29 10:14:34 +0200

    gestion des erreurs script.js

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -6,31 +6,45 @@ const gallery = document.getElementsByClassName("gallery")[0];
 // Récupération des travaux depuis le fichier JSON / API ---------------
 
 const apiWorks = async () => {
-  await fetch("http://localhost:5678/api/works")
-    .then((response) => response.json())
-    .then((worksResponse) => {
-      works = worksResponse;
-    });
+  try {
+    await fetch("http://localhost:5678/api/works")
+      .then((response) => response.json())
+      .then((worksResponse) => {
+        works = worksResponse;
+      });
+  } catch (error) {
+    console.log(
+      "Une erreur s'est produite lors de la récupération des données :",
+      error
+    );
+  }
 };
 
 // Mise en place de l'affichage des travaux ---------------
 
 const displayWorks = async () => {
-  await apiWorks();
-  for (let work of works) {
-    let figure = document.createElement("figure");
-    figure.setAttribute("data-categoryid", work.categoryId);
-    figure.setAttribute("class", "display");
-    let workPicture = document.createElement("img");
-    let figcaption = document.createElement("figcaption");
+  try {
+    await apiWorks();
+    for (let work of works) {
+      let figure = document.createElement("figure");
+      figure.setAttribute("data-categoryid", work.categoryId);
+      figure.setAttribute("class", "display");
+      let workPicture = document.createElement("img");
+      let workFigcaption = document.createElement("figcaption");
 
-    workPicture.setAttribute("src", work.imageUrl);
-    workPicture.setAttribute("alt", work.title);
-    figcaption.innerText = work.title;
+      workPicture.setAttribute("src", work.imageUrl);
+      workPicture.setAttribute("alt", work.title);
+      workFigcaption.innerText = work.title;
 
-    figure.appendChild(workPicture);
-    figure.appendChild(figcaption);
-    gallery.appendChild(figure);
+      figure.appendChild(workPicture);
+      figure.appendChild(workFigcaption);
+      gallery.appendChild(figure);
+    }
+  } catch (error) {
+    console.log(
+      "Une erreur s'est produite lors de l'affichage des travaux' :",
+      error
+    );
   }
 };
 displayWorks();
@@ -41,43 +55,55 @@ let categories = [];
 
 const sectionCategories = document.getElementsByClassName("categories")[0];
 const apiCategories = async () => {
-  await fetch("http://localhost:5678/api/categories")
-    .then((response) => response.json())
-    .then((categoriesResponse) => {
-      categories = categoriesResponse;
-    });
+  try {
+    await fetch("http://localhost:5678/api/categories")
+      .then((response) => response.json())
+      .then((categoriesResponse) => {
+        categories = categoriesResponse;
+      });
+  } catch (error) {
+    console.log(
+      "Une erreur s'est produite lors de l'affichage des catégories :",
+      error
+    );
+  }
 };
 
 const displayCategories = async () => {
-  await apiWorks();
-  await apiCategories();
-  categories.unshift({ name: "Tous" });
+  try {
+    await apiWorks();
+    await apiCategories();
+    categories.unshift({ name: "Tous" });
 
-  for (let category of categories) {
-    let newLi = document.createElement("li");
-    let buttonOfCategories = document.createElement("button");
+    for (let category of categories) {
+      let newLi = document.createElement("li");
+      let buttonOfCategories = document.createElement("button");
 
-    buttonOfCategories.innerText = category.name;
-
-    newLi.appendChild(buttonOfCategories);
-    sectionCategories.appendChild(newLi);
-
-    buttonOfCategories.addEventListener("click", () => {
-      let figures = document.querySelectorAll("figure");
-      for (let figure of figures) {
-        if (category.id !== undefined) {
-          if (
-            parseInt(figure.getAttribute("data-categoryid")) === category.id
-          ) {
-            figure.classList.replace("display", "hidden");
+      buttonOfCategories.innerText = category.name;
+      newLi.appendChild(buttonOfCategories);
+      sectionCategories.appendChild(newLi);
+      buttonOfCategories.addEventListener("click", () => {
+        let figures = document.querySelectorAll("figure");
+        for (let figure of figures) {
+          if (category.id !== undefined) {
+            if (
+              parseInt(figure.getAttribute("data-categoryid")) === category.id
+            ) {
+              figure.classList.replace("display", "hidden");
+            } else {
+              figure.classList.replace("hidden", "display");
+            }
           } else {
             figure.classList.replace("hidden", "display");
           }
-        } else {
-          figure.classList.replace("hidden", "display");
         }
-      }
-    });
+      });
+    }
+  } catch (error) {
+    console.log(
+      "Une erreur s'est produite lors du filtre des catégories :",
+      error
+    );
   }
 };
 displayCategories();
@@ -87,27 +113,34 @@ displayCategories();
 let isConnected = sessionStorage.getItem("isConnected");
 let token = sessionStorage.getItem("token");
 
-async function displayAdminContent() {
-  if (isConnected && token) {
-    const connexionButton = document.querySelector("#btn-login");
-    connexionButton.innerText = "logout";
-    connexionButton.addEventListener("click", function (event) {
-      sessionStorage.clear();
-    });
+const displayAdminContent = async () => {
+  try {
+    if (isConnected && token) {
+      const connexionButton = document.querySelector("#btn-login");
+      connexionButton.innerText = "logout";
+      connexionButton.addEventListener("click", function (event) {
+        sessionStorage.clear();
+      });
 
-    const editionBanner = document.querySelector(".edition");
-    editionBanner.classList.replace("hidden", "display-flex");
-    const editionButton = document.querySelector(".modal-trigger");
-    editionButton.classList.replace("hidden", "display-inlineBlock");
-    const buttonsCategories = document.querySelector(".categories");
-    buttonsCategories.classList.replace("display-flex", "hidden");
+      const editionBanner = document.querySelector(".edition");
+      editionBanner.classList.replace("hidden", "display-flex");
+      const editionButton = document.querySelector(".modal-trigger");
+      editionButton.classList.replace("hidden", "display-inlineBlock");
+      const buttonsCategories = document.querySelector(".categories");
+      buttonsCategories.classList.replace("display-flex", "hidden");
 
-    const sectionIntroduction = document.querySelector("#introduction");
-    const modifButtons = sectionIntroduction.querySelectorAll(".modify-btn");
-    modifButtons.forEach(function (button) {
-      button.classList.replace("hidden", "display-block");
-    });
+      const sectionIntroduction = document.querySelector("#introduction");
+      const modifButtons = sectionIntroduction.querySelectorAll(".modify-btn");
+      modifButtons.forEach(function (button) {
+        button.classList.replace("hidden", "display-block");
+      });
+    }
+  } catch (error) {
+    console.log(
+      "Une erreur s'est produite lors de l'affichage du mode édition :",
+      error
+    );
   }
-}
+};
 
 displayAdminContent();
